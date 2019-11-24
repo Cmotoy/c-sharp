fd053a1fb695a71ee54ee90854701016e8b07d8a
Oluwabunmi Omotoyinbo <cmotoy@outlook.com>
2019-11-23 16:49:49 -0800

    added Variables

---
+++ Variables/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("What is your name");
+            Console.Write("Type your first name");
+            string x;
+            x = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("Type your Last name");
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello," + x + " " + myLastName);
+                Console.ReadLine();
+        }
+    }
+}
